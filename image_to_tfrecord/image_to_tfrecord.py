3d156c67ede6019991480e5cdfa1b8ed2a5620fe
kangda01 <53521184+kangda01@users.noreply.github.com>
2019-08-06 16:30:05 +0800

    创建分批次的tfrecord文件

--- image_to_tfrecord/image_to_tfrecord.py
+++ image_to_tfrecord/image_to_tfrecord.py
@@ -2,16 +2,14 @@ import glob
 import os.path
 import tensorflow as tf
 from PIL import Image
+import math
 
-#@profile
-def image_to_tfrecord(image_path, tfrecord_path):
-    #os.walk() 方法用于通过在目录树中游走输出在目录中的文件名，向上或者向下
+def get_images_dir(image_path):
     sub_dirs = [x[0] for x in os.walk(image_path)][1:]
-    writer = tf.python_io.TFRecordWriter(tfrecord_path)
-    #读取所有的子目录
+    file_list = []
+    lables = []
     for label, sub_dir in enumerate(sub_dirs):
         extensions = ['jpg', 'jpeg']
-        file_list = []
         #os.path.basename函数返回文件名
         dir_name = os.path.basename(sub_dir)
         for extension in extensions:
@@ -20,34 +18,52 @@ def image_to_tfrecord(image_path, tfrecord_path):
             # 定义了文件路径匹配规则，这里可以是绝对路径，也可以是相对路径
             #注意extend和append的区别
             file_list.extend(glob.glob(file_glob))
+            lables.extend([label] * len(glob.glob(file_glob)))
+        #如果file_list为空，跳出本次循环
         if not file_list: continue
+    total_image_numbers = len(file_list)
+    file_lable_list = zip(file_list, lables)
+    return file_lable_list, total_image_numbers
 
-        #处理图片数据
-        for i, file_name in enumerate(file_list):
-            #try...except 添加报错机制，防止图片损坏转换不成功
-            try:
-                #添加显示进度信息
-                print('\r>> Converting image of (%s) %d/%d' % (dir_name, i+1, len(file_list))， end="")
-                #采用PIL模块下的Image读取图片
-                image_raw_data = Image.open(file_name)
-                #获取图片的长宽高，保存在tftecort中，以便还原图片信息使用
-                height = image_raw_data.height
-                width = image_raw_data.width
-                channels = image_raw_data.layers
-                #转换成二进制格式
-                image_raw = image_raw_data.tobytes()
-                example = tf.train.Example(features=tf.train.Features(feature={
-                    'label': _int64_frature(label),
-                    'image_raw': _bytes_feature(image_raw),
-                    'height': _int64_frature(height),
-                    'width': _int64_frature(width),
-                    'channels': _int64_frature(channels)}))
-                writer.write(example.SerializeToString())
-            except IOError as shibai:
-                print('Wrong: ' + file_name)
-                print('Error: ', shibai)
-                print('Skip it\n')
-            # print("转换完成")
+
+#@profile
+def image_to_tfrecord(set_name, image_path, output_path):
+    file_lable_list, total_image_numbers = get_images_dir(image_path)
+    #实现向上取整
+    tfrecords_files_numbers = math.ceil(total_image_numbers / 1000)
+    number = 0
+    #处理图片数据
+    for i, file_name in enumerate(file_lable_list):
+        if not i % 1000:
+            number += 1
+            tfrecord_path = os.path.join(output_path, set_name +
+                                         ('.tfrecords-%.3d-of-%.3d' % (number, tfrecords_files_numbers)))
+
+            writer = tf.python_io.TFRecordWriter(tfrecord_path)
+        #try...except 添加报错机制，防止图片损坏转换不成功
+        try:
+            #添加显示进度信息
+            print('\r>> Converting image  %d/%d' % (i+1, total_image_numbers),end="")
+            #采用PIL模块下的Image读取图片
+            image_raw_data = Image.open(file_name[0])
+            #获取图片的长宽高，保存在tftecort中，以便还原图片信息使用
+            height = image_raw_data.height
+            width = image_raw_data.width
+            channels = image_raw_data.layers
+            #转换成二进制格式
+            image_raw = image_raw_data.tobytes()
+            example = tf.train.Example(features=tf.train.Features(feature={
+                'label': _int64_frature(file_name[1]),
+                'image_raw': _bytes_feature(image_raw),
+                'height': _int64_frature(height),
+                'width': _int64_frature(width),
+                'channels': _int64_frature(channels)}))
+            writer.write(example.SerializeToString())
+        except IOError as shibai:
+            print('Wrong: ' + file_name)
+            print('Error: ', shibai)
+            print('Skip it\n')
+        # print("转换完成")
     writer.close()
 
 
@@ -63,10 +79,14 @@ def main():
     # image_path = r"C:\Users\kangda\Desktop\flower"
     # tfrecord_path = r"C:\Users\kangda\Desktop\flower.tfrecords"
     image_path = r"G:\tensorflow_google\chapter_6\flower_data\flower_photos"
-    tfrecord_path = r"G:\tensorflow_google\chapter_6\flower_data.tfrecords"
-    image_to_tfrecord(image_path, tfrecord_path)
+    output_path = r"G:\tensorflow_google\chapter_6\flower_data"
+    # a, b = get_images_dir(image_path)
+    image_to_tfrecord("training", image_path, output_path)
 
 if __name__ == '__main__':
     main()
 
-
+# image_path = r"G:\tensorflow_google\chapter_6\flower_data\flower_photos"
+# a, b = get_images_dir(image_path)
+# for i, j in enumerate(a):
+#     print (i, j[0])
