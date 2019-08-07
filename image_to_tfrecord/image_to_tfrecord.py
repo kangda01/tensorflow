f5801443bc525334af33980aef962c7da501b805
kangda01 <53521184+kangda01@users.noreply.github.com>
2019-08-07 16:43:53 +0800

    创建分批次的tfrecord文件

--- image_to_tfrecord/image_to_tfrecord.py
+++ image_to_tfrecord/image_to_tfrecord.py
@@ -1,52 +1,48 @@
-import glob
-import os.path
+
+from pathlib import Path
 import tensorflow as tf
 from PIL import Image
 import math
 
 
-'''get_images_dir函数是为了得到每个图片的具体地址，和对应的lable，
-total_image_numbers是为了计算地址下的图片总数，方便后续的命名tfrecord文件和显示操作总进度'''
+'''get_images_dir函数为了得到每个图片地址和对应的标签，同时得到总的图片个数。
+    return:
+        all_image_paths_labels:得到每个图片地址和对应的标签,以便后续读取存储
+        image_count：为了后续显示处理总进度
+'''
 def get_images_dir(image_path):
-    sub_dirs = [x[0] for x in os.walk(image_path)][1:]
-    file_list = []
-    lables = []
-    for label, sub_dir in enumerate(sub_dirs):
-        extensions = ['jpg', 'jpeg']
-        #os.path.basename函数返回文件名
-        dir_name = os.path.basename(sub_dir)
-        for extension in extensions:
-            file_glob = os.path.join(image_path, dir_name, '*.'+extension)
-            #glob.glob(pathname)返回所有匹配的文件路径列表。它只有一个参数pathname，
-            # 定义了文件路径匹配规则，这里可以是绝对路径，也可以是相对路径
-            #注意extend和append的区别
-            file_list.extend(glob.glob(file_glob))
-            lables.extend([label] * len(glob.glob(file_glob)))
-        #如果file_list为空，跳出本次循环
-        if not file_list: continue
-    total_image_numbers = len(file_list)
-    file_lable_list = zip(file_list, lables)
-    return file_lable_list, total_image_numbers
-
+    data_root = Path(image_path)
+    all_image_paths = list(data_root.glob('*/*'))
+    all_image_paths = [str(path) for path in all_image_paths]
+    image_count = len(all_image_paths)
+    # 确定每种图像的标签
+    label_names = sorted(item.name for item in data_root.glob('*/') if item.is_dir())
+    # 为每个标签分配索引
+    label_to_index = dict((name, index) for index, name in enumerate(label_names))
+    # 创建每个图片的标签索引
+    all_image_labels = [label_to_index[Path(path).parent.name]
+                        for path in all_image_paths]
+    all_image_paths_labels = zip(all_image_paths, all_image_labels)
+    return all_image_paths_labels, image_count
 
 #@profile
 def image_to_tfrecord(set_name, image_path, output_path):
-    file_lable_list, total_image_numbers = get_images_dir(image_path)
-    #实现向上取整
-    tfrecords_files_numbers = math.ceil(total_image_numbers / 1000)
+
+    all_image_paths_labels, image_count = get_images_dir(image_path)
+    #实现向上取整,得到总的tfrecord文件个数
+    tfrecords_files_numbers = math.ceil(image_count / 1000)
     number = 0
     #处理图片数据
-    for i, file_name in enumerate(file_lable_list):
+    for i, file_name in enumerate(all_image_paths_labels):
         if not i % 1000:
             number += 1
-            tfrecord_path = os.path.join(output_path, set_name +
-                                         ('-%.3d-of-%.3d.tfrecords' % (number, tfrecords_files_numbers)))
-
-            writer = tf.python_io.TFRecordWriter(tfrecord_path)
+            tfrecord_path = Path(output_path).joinpath(
+                ''.join([set_name, ('-%.3d-of-%.3d.tfrecords' % (number, tfrecords_files_numbers))]))
+            writer = tf.python_io.TFRecordWriter(str(tfrecord_path))
         #try...except 添加报错机制，防止图片损坏转换不成功
         try:
             #添加显示进度信息
-            print('\r>> Converting image  %d/%d' % (i+1, total_image_numbers),end="")
+            print('\r>> Converting image  %d/%d' % (i+1, image_count),end="")
             #采用PIL模块下的Image读取图片
             image_raw_data = Image.open(file_name[0])
             #获取图片的长宽高，保存在tftecort中，以便还原图片信息使用
@@ -66,7 +62,6 @@ def image_to_tfrecord(set_name, image_path, output_path):
             print('Wrong: ' + file_name)
             print('Error: ', shibai)
             print('Skip it\n')
-        # print("转换完成")
     writer.close()
 
 
@@ -79,10 +74,11 @@ def _bytes_feature(value):
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
 
 def main():
+
     image_path = r"G:\tensorflow_google\chapter_6\flower_data\flower_photos"
     output_path = r"G:\tensorflow_google\chapter_6\flower_data"
     image_to_tfrecord("training", image_path, output_path)
 
 if __name__ == '__main__':
     main()
-
+    
